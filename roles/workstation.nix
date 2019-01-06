e9c463af317981dc4fa5cd149b51506c85c16465
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-01-06 22:17:13 +0300

    Update ssh config

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -3,38 +3,5 @@
 {
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-
   system.autoUpgrade.enable = true;
-
-  programs.ssh.matchBlocks = {
-    "github.com" = {
-      hostname = "github.com";
-      user = "git";
-      identityFile = "~/.ssh/id_rsa";
-    };
-
-    "bitbucket.org" = {
-      hostname = "bitbucket.org";
-      user = "git";
-      identityFile = "~/.ssh/id_rsa";
-    };
-
-    "ksazdp" = {
-      hostname = "10.101.102.130";
-      user = "deployer";
-      identityFile = "~/.ssh/id_rsa";
-    };
-
-    "ksazds" = {
-      hostname = "10.101.102.250";
-      user = "deployer";
-      identityFile = "~/.ssh/id_rsa";
-    };
-
-    "dedimax" = {
-      hostname = "91.134.122.220";
-      user = "root";
-      identityFile = "~/.ssh/id_rsa";
-    };
-  };
 }
