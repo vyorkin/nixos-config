32dc9deff1298971206edd88bad57563eb9f4d20
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-01-08 04:24:27 +0300

    Set updateResolveConfig = true

--- cfgs/vpn.nix
+++ cfgs/vpn.nix
@@ -6,8 +6,7 @@
       home = {
         config = ''config /etc/nixos/home/dotsecrets/vpn/home.ovpn'';
 	autoStart = false;
-        up = "echo nameserver $nameserver | ${pkgs.openresolv}/sbin/resolvconf -m 0 -a $dev";
-        down = "${pkgs.openresolv}/sbin/resolvconf -d $dev";
+	updateResolvConf = true;
       };
     };
   };
