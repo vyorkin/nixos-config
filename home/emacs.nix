701cab89c8c53531d7719e30f2e2e9c744ebb39a
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2019-05-14 20:15:59 +0300

    Update emacs config

--- home/emacs.nix
+++ home/emacs.nix
@@ -2,6 +2,19 @@
 
 {
   home.file = {
-    "emacs.d" = { source = ./emacs.d; };
+    "init.el" = {
+      source = ./dotfiles/emacs/init.el;
+      target = ".emacs.d/init.el";
+    };
+
+    "config.org" = {
+      source = ./dotfiles/emacs/config.org;
+      target = ".emacs.d/config.org";
+    };
+
+    "yasnippet-snippets" = {
+      source = ./dotfiles/emacs/yasnippet-snippets;
+      target = ".emacs.d/yasnippet-snippets";
+    };
   };
 }
