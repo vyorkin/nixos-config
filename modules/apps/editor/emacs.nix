78cff37b488c8e2fea98926c6fceaa5ffb40c76f
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2021-11-02 22:34:38 +0300

    Start emacs as a user daemon

--- modules/apps/editor/emacs.nix
+++ modules/apps/editor/emacs.nix
@@ -7,6 +7,15 @@
       package = pkgs.emacsPgtk;
     };
 
+    services.emacs = {
+      enable = true;
+      package = pkgs.emacsPgtk;
+    };
+
+    home.packages = with pkgs; [
+      ispell
+    ];
+
     systemd.user.services.emacs.Service.Environment =
       "PATH=/run/current-system/sw/bin:/etc/profiles/per-user/vyorkin/bin";
   };
